2a9fe18a6cea1dcd64fca668aac9f38b7757ab90
mwc7 <mwc7@maroon21.cs.calvin.edu>
2020-01-17 11:51:36 -0500

    Ruined it

--- tornado.js
+++ tornado.js
@@ -1,12 +1,4 @@
 let time = 0
-d3.csv('tornadoPressureData1.csv')
-    .then(function(data) {
-
-    })
-    .catch(function(error) {
-
-    })
-
 let data = [0,1,2,3];
 
 function slideTime() {
@@ -14,4 +6,28 @@ function slideTime() {
         .text(`The time is ${time}`)
 }
 
-d3.select("input#myRange")
+var sliderSimple = d3.select("body")
+    .select("div#slider")
+    .append("input")
+    .attr("type", "range")
+    .attr("id", "time-slider")
+    .sliderBottom()
+    .min(d3.min(data))
+    .max(d3.max(data))
+    .width(300)
+    .tickFormat(d3.format('.2%'))
+    .ticks(5)
+    .default(0.015)
+    .on('onchange', slideTime(value));
+
+var gSimple = d3
+    .select('div#time-slider')
+    .append('svg')
+    .attr('width', 500)
+    .attr('height', 100)
+    .append('g')
+    .attr('transform', 'translate(30,30)');
+
+gSimple.call(sliderSimple);
+
+d3.select('p#time-value').text(d3.format('.2%')(sliderSimple.value()));
